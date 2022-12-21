02ca4cf1350610e521f13eb5989a83cfd9363024
Diogo Paiva Pereira <diogo.p.pereira@unesp.br>
2022-12-21 03:22:23 -0300

    Solução e alterações completas

---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    width: 80%;
+
+    h3 {
+        font-size: 32px;
+        color: #BFBFBF;
+    }
+
+    p {
+        font-size:16px;
+        color: #FAFAFA60;
+        margin-bottom:10px;
+    }
+
+    phttps{
+        font-size:16px;
+        color: #FAFAFA60;
+        margin-bottom:10px;
+    }
+
+    pssh{
+        font-size:16px;
+        color: #FAFAFA60;
+        margin-bottom:20px;
+    }
+
+    a.mostrar {
+        color: #15BEBB;
+    }
+
+    a.remover {
+        color: #EA2727;
+        margin-top:20px;
+        margin-bottom:10px;
+    }
+
+    hr {
+        color: #FAFAFA60;
+        margin: 20px 0;
+    }
+`
